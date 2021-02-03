a7c1398b15f30c80badb47945c25d99414f5acf6
India Kato <india.kato@gmail.com>
2021-02-02 19:05:32 -0800

    adding http and dio dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 import 'login_page.dart';
 
-void main() => runApp(MyApp());
+Future main() async {
+  // await DotEnv.load(fileName: '.env');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,7 +19,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
 }
+
+// class SwipePage extends StatefulWidget {
+//   final dio = Dio()
+//   _SwipePageState createState() => _SwipePageState();
+// }
+//
+// class _SwipePageState extends State<SwipePage> {
+//   void restaurantResults(String query) async {
+//     final response = await widget.dio.get('https://api.yelp.com/v3/businesses/search'), queryParameters: {
+//
+//     }
+//   }
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//
+//     );
+//   }
+// }
