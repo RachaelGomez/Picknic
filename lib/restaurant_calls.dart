adc300160ff7b57dc62326f002c10542fa325679
India Kato <india.kato@gmail.com>
2021-02-15 16:21:50 -0800

    Swiping working and cards populating

--- lib/restaurant_calls.dart
+++ lib/restaurant_calls.dart
@@ -1,22 +1,37 @@
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'package:picknic/Models/business.dart';
 
-Future<http.Response> fetchRestaurantList (String url, String groupName) async {
-  Map data = {
-    'group_name': groupName,
-  };
-  //encode Map to JSON
-  var body = json.encode(data);
+const String API_KEY = '_iDM48sJ0J6NzjPXTGMaFm4wGc3mIWuSyBEtbAZ0IkwFlbwQ_g9mt3l9384Ng41dCpJbsK7-55u8vBvlZgCQb7qPE_xh2LMhatkeoq4cXLCK5KyHHhVOk52UtYcYYHYx';
+const Map<String, String> AUTH_HEADER = {"Authorization": "Bearer $API_KEY"};
 
-  var response = await http.get('http://localhost:3000/groups/$groupName',
-    headers: {"Content-Type": "application/json"},
-  );
+// Future<http.Response> fetchRestaurantList (String url, String groupName) async {
+//   Map data = {
+//     'group_name': groupName,
+//   };
+//   //encode Map to JSON
+//   var body = json.encode(data);
+//
+//   var response = await http.get('http://localhost:3000/groups/$groupName',
+//     headers: {"Content-Type": "application/json"},
+//   );
+//
+//
+//   print("${response.statusCode}");
+//   print("${response.body}");
+//
+//   return response;
+// };
 
-
-  print("${response.statusCode}");
-  print("${response.body}");
-
-  return response;
-}
-
+// Future<Business> fetchBusiness(businessId) async {
+//   final response = await http.get('https://api.yelp.com/v3/businesses/' + businessId, headers: AUTH_HEADER);
+//
+//   if (response.statusCode == 200) {
+//     print(Business.fromJson(jsonDecode(response.body)));
+//     return Business.fromJson(jsonDecode(response.body));
+//   } else {
+//     throw Exception('Failed to load business');
+//   }
+//
+// }
--- lib/screens/swiping_screen.dart
+++ lib/screens/swiping_screen.dart
@@ -1,30 +1,33 @@
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tindercard/flutter_tindercard.dart';
 import 'package:picknic/first_screen.dart';
 import 'package:picknic/screens/host_screen.dart';
 import 'package:http/http.dart' as http;
+import 'package:picknic/Models/business.dart';
 
 class SwipeScreen extends StatefulWidget {
-  final List restaurantIds;
-  const SwipeScreen({Key key, this.restaurantIds}) : super(key: key);
+  final List restaurants;
+  const SwipeScreen({Key key, this.restaurants}) : super(key: key);
   @override
   _SwipeScreenState createState() => _SwipeScreenState();
 }
 
 class _SwipeScreenState extends State<SwipeScreen> {
+  static const String API_KEY = '_iDM48sJ0J6NzjPXTGMaFm4wGc3mIWuSyBEtbAZ0IkwFlbwQ_g9mt3l9384Ng41dCpJbsK7-55u8vBvlZgCQb7qPE_xh2LMhatkeoq4cXLCK5KyHHhVOk52UtYcYYHYx';
+  static const Map<String, String> AUTH_HEADER = {"Authorization": "Bearer $API_KEY"};
+
   @override
   void initState() {
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     CardController controller;
     return Scaffold(
-      appBar: AppBar(title: const Text('Start Swiping'), actions: <Widget>[
+      appBar: AppBar(title: Text('Start Swiping'), actions: <Widget>[
         IconButton(
           icon: const Icon(Icons.home),
           onPressed: () {
@@ -43,25 +46,78 @@ class _SwipeScreenState extends State<SwipeScreen> {
         child: Container(
           height: MediaQuery.of(context).size.height * 0.6,
           child: new TinderSwapCard(
-              cardBuilder: (context, index) => Card(
-                child: Text(widget.restaurantIds[index]),
+              cardBuilder: (context, index) => Container(
+                alignment: Alignment.center,
+                child: Card(
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: <Widget> [
+                        ListTile(
+                          title: Text(
+                              widget.restaurants[index]["restaurant_name"],
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red)),
+                        ),
+                        Column(
+                          children: <Widget> [
+                            ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(8.0),
+                                  topRight: Radius.circular(8.0),
+                                  bottomLeft: Radius.circular(8.0),
+                                  bottomRight: Radius.circular(8.0)
+                              ),
+                              child: Image.network(
+                                widget.restaurants[index]["image_url"],
+                                width: 300,
+                                height: 200,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                                height: 50
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text('Categories:', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                                    for (var aliasTitle in widget.restaurants[index]["categories"]) Text(aliasTitle["title"]),
+                                  ],
+                                ),
+                                Column(
+                                  children: <Widget> [
+                                    for (var transaction in widget.restaurants[index]["transactions"]) Text(transaction.toUpperCase()),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                ),
               ),
-              totalNum: widget.restaurantIds.length,
+              totalNum: widget.restaurants.length,
               swipeUp: true,
               swipeDown: true,
               orientation: AmassOrientation.BOTTOM,
               stackNum: 3,
               swipeEdge: 4.0,
               maxWidth: MediaQuery.of(context).size.width * 0.9,
-              maxHeight: MediaQuery.of(context).size.width * 0.9,
+              maxHeight: MediaQuery.of(context).size.width * 3.0,
               minWidth: MediaQuery.of(context).size.width * 0.8,
               minHeight: MediaQuery.of(context).size.width * 0.8,
               cardController: controller = CardController(),
-              swipeUpdateCallback:
-                  (DragUpdateDetails details, Alignment align) {
-                if (align.x < 0) {
+              swipeCompleteCallback:
+                  (CardSwipeOrientation orientation, int index) {
+                if (orientation == CardSwipeOrientation.LEFT) {
                   print("card is swiping left");
-                } else if (align.x > 0) {
+                } else if (orientation == CardSwipeOrientation.RIGHT) {
                   print("card is swiping right");
                 }
               }
@@ -70,7 +126,9 @@ class _SwipeScreenState extends State<SwipeScreen> {
       ),
     );
   }
-  
+
+
+
 }
 
 
